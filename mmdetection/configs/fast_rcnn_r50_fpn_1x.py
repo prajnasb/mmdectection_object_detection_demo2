34142315822c90bb5ad2973a4511b830c4f583e9
prajnasb <61310612+prajnasb@users.noreply.github.com>
2020-04-27 16:54:38 +0530

    Update fast_rcnn_r50_fpn_1x.py

--- mmdetection/configs/fast_rcnn_r50_fpn_1x.py
+++ mmdetection/configs/fast_rcnn_r50_fpn_1x.py
@@ -54,8 +54,8 @@ test_cfg = dict(
     rcnn=dict(
         score_thr=0.05, nms=dict(type='nms', iou_thr=0.5), max_per_img=100))
 # dataset settings
-dataset_type = 'CocoDataset'
-data_root = 'data/coco/'
+dataset_type = 'VOCDataset'
+data_root = 'data/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
