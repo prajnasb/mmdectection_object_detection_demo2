9611cb6b7682e6acced7f201f2b8cdfb8dbb1fd7
prajnasb <61310612+prajnasb@users.noreply.github.com>
2020-04-28 14:28:37 +0530

    Update voc.py

--- mmdetection/mmdet/datasets/voc.py
+++ mmdetection/mmdet/datasets/voc.py
@@ -6,7 +6,7 @@ from .xml_style import XMLDataset
 @DATASETS.register_module
 class VOCDataset(XMLDataset):
 
-    CLASSES = ('with_mask','without_mask')
+    CLASSES = ('fire_extinguisher')
 
     def __init__(self, **kwargs):
         super(VOCDataset, self).__init__(**kwargs)
